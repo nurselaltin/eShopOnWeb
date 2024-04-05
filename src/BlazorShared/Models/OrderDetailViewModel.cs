9f1b751e8dea2349f6e2440d0a4ce53f8fbbf2e6
nurselaltin <nurselaltin.na@gmail.com>
2024-04-06 00:38:03 +0300

    Adding order's model, pages, etc.

---
+++ src/BlazorShared/Models/OrderDetailViewModel.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace BlazorShared.Models;
+
+public class OrderDetailViewModel : OrderViewModel
+{
+    public List<OrderItemViewModel> OrderItems { get; set; } = new();
+}
